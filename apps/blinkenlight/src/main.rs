1d5abdeed4d931c04d3e922b78aecd564197047e
Jeff Glaum <jeffglaum@live.com>
2024-09-14 13:41:32 -0700

    blink user LED on PB8

--- apps/blinkenlight/src/main.rs
+++ apps/blinkenlight/src/main.rs
@@ -3,16 +3,35 @@
 
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 
+use hal::prelude::*;
+use hal::stm32;
+use stm32g4xx_hal as hal;
+
 use cortex_m_rt::entry;
-extern crate stm32g4xx_hal as hal;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    // Initialze RTT debug message interface
+    let dp = stm32::Peripherals::take().expect("ERROR: cannot take peripherals");
+    let mut rcc = dp.RCC.constrain();
+
+    // Initialize RTT debug message interface
     rtt_init_print!();
 
+    // Initialize LED GPIO
+    rprintln!("INFO: initialize LED");
+    let gpiob = dp.GPIOB.split(&mut rcc);
+    let mut led = gpiob.pb8.into_push_pull_output();
+
     loop {
-        rprintln!("Hello World!");
+        rprintln!("INFO: set LED low");
+        for _ in 0..10_000 {
+            led.set_low().unwrap();
+        }
+
+        rprintln!("INFO: set LED high");
+        for _ in 0..10_000 {
+            led.set_high().unwrap();
+        }
     }
 }
