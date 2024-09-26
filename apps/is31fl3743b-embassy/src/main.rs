2e400bf496f39a198561041b5100d56b0ef92e32
Jeff Glaum <jeffglaum@live.com>
2024-09-25 20:56:25 -0700

    simplified method to obtain spidevice

--- apps/is31fl3743b-embassy/src/main.rs
+++ apps/is31fl3743b-embassy/src/main.rs
@@ -3,20 +3,14 @@
 
 use panic_halt as _;
 
-use core::cell::RefCell;
 use cortex_m_rt::entry;
-use embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice;
 use embassy_stm32::dma::NoDma;
 use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_stm32::spi::{BitOrder, Config, Spi, MODE_0};
 use embassy_stm32::time::Hertz;
-use embassy_sync::blocking_mutex::NoopMutex;
-//use embedded_hal::spi::Operation::DelayNs;
-//use embedded_hal_async::spi::Operation::DelayNs;
-//use embedded_hal::delay::DelayNs;
+use embedded_hal_bus::spi::ExclusiveDevice;
 use is31fl3743b_driver::{CSy, Is31fl3743b, SWx};
 use rtt_target::{rprintln, rtt_init_print};
-use static_cell::StaticCell;
 #[entry]
 fn main() -> ! {
     let p = embassy_stm32::init(Default::default());
@@ -28,28 +22,13 @@ fn main() -> ! {
     spi_config.mode = MODE_0;
     spi_config.bit_order = BitOrder::MsbFirst;
     spi_config.frequency = Hertz(1_000_000);
-    // Spi<'_, {unknown}, NoDma, _>
-    // Spi<'d, T, Tx, Rx>
-    static SPI_BUS: StaticCell<
-        NoopMutex<RefCell<Spi<'_, embassy_stm32::peripherals::SPI1, NoDma, NoDma>>>,
-    > = StaticCell::new();
 
     let spi = Spi::new(p.SPI1, p.PB3, p.PB5, p.PB4, NoDma, NoDma, spi_config);
-    let spi_bus = NoopMutex::new(RefCell::new(spi));
-    let spi_bus = SPI_BUS.init(spi_bus);
-
     let cs = Output::new(p.PA11, Level::High, Speed::VeryHigh);
 
-    let spi_dev = SpiDevice::new(spi_bus, cs);
+    let spi_dev = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
 
     let mut driver = Is31fl3743b::new(spi_dev).unwrap();
-
-    // Perform a test which detects shorted LEDs
-    //let delay = DelayNs;
-    //let led_status = driver.detect_shorts(d).unwrap();
-    //if !led_status.all_leds_ok() {
-    //panic!("There is a short among the LEDs")
-    //}
 
     // Perform some configuration
     let _ = driver.enable_phase_delay();
